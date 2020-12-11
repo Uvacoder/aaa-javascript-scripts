4419abddb5a6fe09d4dba61218fa1f2d793ad99f
slowy07 <slowy.arfy@gmail.com>
2020-12-12 06:20:14 +0700

    data types

---
+++ basic/dataTypes.js
@@ -0,0 +1,12 @@
+//undifined, null, boolean, string, symbol, number, object
+
+
+var name = "arfyslowy";
+getNumber = 8;
+let teamName = "kloterSurga";
+const pi = 3.14;
+
+console.log(name);
+console.log(getNumber);
+console.log(teamName);
+console.log(pi);
