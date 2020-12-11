18b666485493ad50b43e83f21c10531c9b340769
slowy07 <slowy.arfy@gmail.com>
2020-12-12 06:51:22 +0700

    more javascript data types

---
+++ basic/moreJavascriptDataTypes.js
@@ -0,0 +1,23 @@
+var getLength = 12;
+var myName = "arfy slowy";
+var res = {firstName: "arfy", lastName: "slowy"}; //object
+
+var singleQuote = "it's Name jole";
+var singleQuote2 = "name is 'jole'";
+var doubleQuote = 'game name "black paradise"';
+
+console.log(doubleQuote);
+
+
+//javascript numbers
+var number1 = 14.21;
+var number2 = 32;
+var scientific = 2e+5 //200000
+var scientific2 = 2e-5 // 0.00002
+
+//javascript booleans
+var x = 5;
+var y = 5;
+var z = 6;
+//(x == y) return True
+//(x == z) return False
