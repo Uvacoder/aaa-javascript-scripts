8f936b6754dfa4acc010d3baed11dc846ada4783
slowy07 <slowy.arfy@gmail.com>
2020-12-12 06:56:53 +0700

    moreJavascriptDataTypes02

---
+++ basic/moreJavascriptDataTypes02.js
@@ -0,0 +1,24 @@
+var ourName = ["arfy slowy", "zulkeprets","aditya"];
+var personInformation = {firstName : "zulkipli", age: 50, eyeColor: 'cyan'};
+
+
+/*
+typeof operator
+
+returns string
+typeof ""
+typeof "arfy"
+typeof "arfy slowy"
+
+
+returns number
+typeof 0
+typeof 314
+typeof 3.14
+typeof (3)
+typeof (3 + 4)
+
+undefinedVar
+var personName;
+*/
+console.log(ourName)
