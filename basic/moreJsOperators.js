6e281bc37578adb7bf0a4ab3c404d4eb81bed364
slowy07 <slowy.arfy@gmail.com>
2020-12-12 06:50:55 +0700

    javascript operators

---
+++ basic/moreJsOperators.js
@@ -0,0 +1,48 @@
+var name = "arfy";
+var backName = "slowy";
+var fullName = name+" "+backName;
+
+
+var textName = "a";
+txtName += " long day";
+
+var x = 5 + 5;
+var y = "5" + 5;
+var z = "day " + 12;
+
+
+/*
+javascript comparison operator
+
+==	equal to
+===	equal value and equal type
+!=	not equal
+!==	not equal value or not equal type
+>	greater than
+<	less than
+>=	greater than or equal to
+<=	less than or equal to
+?	ternary operator
+
+javascript logical operators
+
+&&	logical and
+||	logical or
+!	logical not
+
+
+javascript type operators
+
+typeof	Returns the type of a variable
+instanceof	Returns true if an object is an instance of an object type
+
+
+javascript bitwise operators
+&	AND	5 & 1	0101 & 0001	0001	 1
+|	OR	5 | 1	0101 | 0001	0101	 5
+~	NOT	~ 5	 ~0101	1010	 10
+^	XOR	5 ^ 1	0101 ^ 0001	0100	 4
+<<	Zero fill left shift	5 << 1	0101 << 1	1010	 10
+>>	Signed right shift	5 >> 1	0101 >> 1	0010	  2
+>>>	Zero fill right shift	5 >>> 1	0101 >>> 1	0010	  2
+*/
