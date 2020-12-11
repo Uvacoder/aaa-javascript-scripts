6f93985180d1150572736df8cb13b07d704cb77e
slowy07 <slowy.arfy@gmail.com>
2020-12-12 06:31:21 +0700

    operators

---
+++ basic/operators.js
@@ -0,0 +1,23 @@
+
+var number1 = 9;
+var number2 = 12;
+
+//add operators
+var add = number1 + number2;
+var multiply = number1 * number2;
+var subtract = number1 - number2;
+var dividing = number1 / number2;
+var remainder = number1 % number2;
+/*
+increment
+number1++
+var increment = number1;
+
+decrement
+number1--
+var decrement = number1;
+*/
+var exponent = number1 ** 2; //can using Math.pow(number,numberPow)
+var multiple = number1 + (2 * 3) 4 - 2 + 13 +2;
+
+console.log(add); //change
