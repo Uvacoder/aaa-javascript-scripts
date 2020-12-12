69e559755996c0628ff2fbf0447c0cac28dfe7e6
slowy07 <slowy.arfy@gmail.com>
2020-12-13 06:56:52 +0700

    date to string

---
+++ conversion/dateToString.js
@@ -0,0 +1,15 @@
+String(Date()) //return day month date year hour,min,sec locationTime
+
+/*
+
+getDate()	Get the day as a number (1-31)
+getDay()	Get the weekday a number (0-6)
+getFullYear()	Get the four digit year (yyyy)
+getHours()	Get the hour (0-23)
+getMilliseconds()	Get the milliseconds (0-999)
+getMinutes()	Get the minutes (0-59)
+getMonth()	Get the month (0-11)
+getSeconds()	Get the seconds (0-59)
+getTime()	Get the time (milliseconds since January 1, 1970)
+
+*/
