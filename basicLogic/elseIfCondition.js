898026cb206302832090d584af3d840bc3f95cb3
slowy07 <slowy.arfy@gmail.com>
2020-12-12 07:03:47 +0700

    else if condition

---
+++ basicLogic/elseIfCondition.js
@@ -0,0 +1,28 @@
+/*
+if(condition)
+{
+  statement
+}
+else if(condition)
+{
+  statement
+}
+else
+{
+  statement
+}
+*/
+
+hour = 12
+if(hour < 18)
+{
+  say = "good day";
+}
+else if(hour > 18)
+{
+  say = "med day";
+}
+else
+{
+  say = "long day"
+}
