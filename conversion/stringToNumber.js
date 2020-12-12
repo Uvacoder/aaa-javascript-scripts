a9f045c69a890655490701f2fc7ec6f19607692a
slowy07 <slowy.arfy@gmail.com>
2020-12-13 06:49:38 +0700

    string to number conversion

---
+++ conversion/stringToNumber.js
@@ -0,0 +1,11 @@
+Number("3.14"); 
+Number(" "); //return 0
+Number(""); //return 0
+Number("99 88"); //return NaN
+console.log(Number("12"));
+
+
+/*
+parseFloat()	Parses a string and returns a floating point number
+parseInt()	Parses a string and returns an integer
+*/
