68ff8cb328499789435a7a155f0cbadabbaa6ed4
slowy07 <slowy.arfy@gmail.com>
2020-12-13 06:39:04 +0700

    get all of data type

---
+++ conversion/typeOf.js
@@ -0,0 +1,10 @@
+typeof "arfy" //string
+typeof 3.14         //return number
+typeof NaN          //return number
+typeof false                    //return boolean
+typeof [1,2,3,5]                //return object
+typeof {name: "arfy", id: 2231} //return object
+typeof new Date()               //return object
+typeof function() {}            //return function
+typeof myName                   //return undefined
+typeof null                     //return object
