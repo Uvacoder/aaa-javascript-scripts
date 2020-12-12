6b1d735744c719945ad802a5f919a7a554bd4f73
slowy07 <slowy.arfy@gmail.com>
2020-12-12 07:10:17 +0700

    switch conditions

---
+++ basicLogic/switchCondition.js
@@ -0,0 +1,39 @@
+/*
+switch(expression) {
+  case x:
+    // code block
+    break;
+  case y:
+    // code block
+    break;
+  default:
+    // code block
+}
+*/
+
+switch (new Date().getDay())
+{
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+     day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case 6:
+    day = "Saturday";
+    break;
+  default:
+    day = "weekend";
+  }
