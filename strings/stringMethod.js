253adc3e589dd49113eea7a4b5903dee54bda03c
slowy07 <slowy.arfy@gmail.com>
2020-12-14 05:59:26 +0700

    string method

---
+++ strings/stringMethod.js
@@ -0,0 +1,24 @@
+var message = "sayuti tell jol, he can't coding";
+
+//getLength
+var getLength = message.length;
+//indexOf()
+var findingString = message.indexOf("tell");
+//lastIndexOf()
+var returnIndex = message.lastIndexOf("tell");
+//search()
+var searchMessage = message.search("tell");
+
+//slice() method
+var stringMessage = "Apple, Banana, Kiwi";
+var result = stringMessage.slice(7, 13);
+
+//substr() method
+var stringMessage1 = "Apple, Banana, Kiwi";
+var result1 = stringMessage1.substr(7,6);
+
+/*
+using console.log(variable) to output each one
+or
+using alert(variable) to output each one
+*/
