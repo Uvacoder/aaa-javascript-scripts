454dca87bae46f8c108a8cbc6ff8c5edd1ae66a1
slowy07 <slowy.arfy@gmail.com>
2020-12-13 16:47:45 +0700

    string searching

---
+++ regExpression/stringSearch.js
@@ -0,0 +1,12 @@
+//using search()
+
+var namePerson = "arfy slowy";
+var searching = namePerson.search("arfy");
+
+console.log(searching);
+
+/*
+var namePerson = "arfy slowy";
+var searching = namePerson.search(/slowy/i);
+console.log(searching);
+*/
