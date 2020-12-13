e24245abbb86806726ebd579644de97b685ad9cd
slowy07 <slowy.arfy@gmail.com>
2020-12-13 16:47:13 +0700

    string replace

---
+++ regExpression/stringReplace.js
@@ -0,0 +1,11 @@
+var personName = "arfy slowy";
+var replaceName = personName.replace("arfy", "jole");
+console.log(replaceName);
+
+
+
+/*
+var personName = "arfy slowy";
+var replaceName = personName.replace(/arfy/i, "jole");
+console.log(replaceName);
+*/
