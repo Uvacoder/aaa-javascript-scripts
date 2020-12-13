1ed264dde56faa017444036fbd32b2f0eae3bc44
slowy07 <slowy.arfy@gmail.com>
2020-12-14 06:20:15 +0700

    example code string method

---
+++ strings/stringMethod2.js
@@ -0,0 +1,26 @@
+var stringMessage = "jo now work on reach company";
+
+//replace()
+var replaceString = stringMessage.replace("reach", "google");
+/*
+using /i for case insensitive replace("reach/i", "google");
+using /g for replace global replace("reach/g", "google");
+*/
+
+
+//toUpperCase()
+var toUpper = stringMessage.toUpperCase();
+//toLowerCase()
+var toLower = stringMessage.toLowerCase();
+//concat()
+var joinString = stringMessage.concat(" "," and work as programmer");
+//trim()
+var trimMessage = stringMessage.trim();
+//charAt()
+var getIndex = stringMessage.charAt(1);
+
+/*
+using console.log(variable) to output each one
+or
+using alert(variable) to output each one
+*/
