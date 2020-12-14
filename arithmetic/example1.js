855c591ad78293668191345a51cde9149de5b9a3
slowy07 <slowy.arfy@gmail.com>
2020-12-15 06:23:00 +0700

    arithmetic

---
+++ arithmetic/example1.js
@@ -0,0 +1,34 @@
+var numberVariable = 5;
+var numberVariable2 = 3;
+
+//adding
+var adding  = numberVariable + numberVariable2;
+
+//subracting
+var subtract = numberVariable - numberVariable2;
+//multiply
+var multiply = numberVariable * numberVariable2;
+//dividing
+var divide = numberVariable / numberVariable2;
+//exponation
+var exponent = numberVariable ** numberVariable2;
+//var exponent = Math.pow(numberVariable, numberVariable2);
+
+
+/*
+increment
+numberVariable++;
+console.log(numberVariable);
+*/
+
+/*
+decrement
+numberVariable--;
+console.log(numberVariable--);
+*/
+
+console.log(exponent);
+console.log(divide);
+console.log(multiply);
+console.log(subract);
+console.log(adding);
