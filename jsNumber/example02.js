cfbf6def07218ec15a6f90b6ddd285ed57657809
slowy07 <slowy.arfy@gmail.com>
2020-12-15 06:42:48 +0700

    adding example

---
+++ jsNumber/example02.js
@@ -0,0 +1,11 @@
+var number1 = "20";
+var number2 = 30;
+
+
+
+//adding string
+var result1 = number1 + number2; //will be 2030
+var result2 = number2 + number1; //will be 3020
+
+console.log(result1);
+console.log(result2);
