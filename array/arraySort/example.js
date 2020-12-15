23fdaac9c900e337fd19f336993ed54a627a788f
slowy07 <slowy.arfy@gmail.com>
2020-12-16 06:45:56 +0700

    array sort

---
+++ array/arraySort/example.js
@@ -0,0 +1,9 @@
+var ourTeam = ["arfy slowy", "zulfikri", "catur rivaldi", "sayuti"];
+var teamId =[1,40,25,30];
+
+//sort array alphabetically
+ourTeam.sort();
+
+//reverse array
+ourTeam.sort()
+ourTeam.reverse()
