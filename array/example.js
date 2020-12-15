4585c1ce86225014f9a0422e3b739292d46af9d0
slowy07 <slowy.arfy@gmail.com>
2020-12-15 14:08:15 +0700

    example

---
+++ array/example.js
@@ -0,0 +1,19 @@
+var carsModel = ['volvo', 'bmw', 'cadillac'];
+console.log(carsModel[1]); //bmw
+
+
+
+//length property
+carsModel.length;
+//sort array
+carsModel.sort();
+//adding element
+carsModel.push("lamborghini");
+//add element using length property
+carsModel[carsModel.length] = "mercedes";
+//recognize array
+typeof carsModel;
+
+
+//accessing last array elements
+console.log(carsModel[carsModel.length -1])
