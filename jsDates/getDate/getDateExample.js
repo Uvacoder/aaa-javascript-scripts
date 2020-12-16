dd9327239df59db11c3c40cc50b801084fd98cc3
slowy07 <slowy.arfy@gmail.com>
2020-12-16 07:34:08 +0700

    get date example

--- jsDates/getDate/getDateExample.js
+++ jsDates/getDate/getDateExample.js
@@ -7,3 +7,13 @@ console.log(myDate.getTime());
 console.log(myDate.getFullYear());
 //getMonth()
 console.log(myDate.getMonth());
+//getDate
+console.log(myDate.getDate());
+//getHours
+console.log(myDate.getHours());
+//getSeconds()
+console.log(myDate.getSecond());
+//getMilliseconds()
+console.log(myDate.getMilliseconds());
+//getDay()
+console.log(myDate.getDay());
