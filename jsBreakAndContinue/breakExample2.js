33019ca9d99cc76c1adbb6f37aeecac0dd385efc
slowy07 <slowy.arfy@gmail.com>
2020-12-16 13:44:06 +0700

    break example

---
+++ jsBreakAndContinue/breakExample2.js
@@ -0,0 +1,9 @@
+var ourTeam = ["arfy","zul","sayuti","adit","catur"]
+list:{
+  text += ourTeam[0] + "<br>";
+  text += ourTeam[1] + "<br>";
+  text += ourTeam[2] + "<br>";
+  break list;
+  text += ourTeam[3] + "<br>";
+  text += ourTeam[4] + "<br>";
+} 
