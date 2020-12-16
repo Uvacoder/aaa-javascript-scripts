b10b6f69c57b7881ff2bba52139314c621f76394
slowy07 <slowy.arfy@gmail.com>
2020-12-16 07:19:52 +0700

    date format example 1

---
+++ jsDates/dateFormat/dateFormatExample.js
@@ -0,0 +1,17 @@
+//complete date
+var myDate = new Date("2020-05-10");
+
+//ISO date
+var myDate2 = new Date("2020-05");
+var myDate02 = new Date("2020"); //ISO date only year
+
+//short date
+var myDate3 = new Date("10/25/2020"); //day/month/year
+
+//long date
+var myDate4 = new Date("Apr 03 2020");
+var myDate04 = new Date("03 January 2020");
+
+console.log(myDate);
+console.log(myDate3);
+console.log(myDate4);
