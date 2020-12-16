3d0a5a0708ee09d9bcc8eda8f1281c2d4dc78bd7
slowy07 <slowy.arfy@gmail.com>
2020-12-16 07:00:56 +0700

    example date

---
+++ jsDates/exampleDate.js
@@ -0,0 +1,13 @@
+var myDate = new Date();
+
+/*
+four way to create date object
+new Date();
+new Date(year, month, day, hours, minutes, seconds, milliseconds);
+new Date(milliseconds);
+new Date(date string);
+*/
+
+var myDate2 = new Date(2020, 12 , 2, 3, 4, 5, 0);
+
+console.log(myDate2);
