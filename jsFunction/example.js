530d9865a34e46bc6c06cfba1289b6d9fec3d6e2
slowy07 <slowy.arfy@gmail.com>
2020-12-16 13:32:47 +0700

    javascript function

---
+++ jsFunction/example.js
@@ -0,0 +1,14 @@
+/*
+function name(expression)
+{
+  statement here
+  return data;
+}
+*/
+function addingNumber(number1, number2)
+{
+  return number1 + number2;
+}
+var getAddingNumber = addingNumber(3,2);
+console.log(getAddingNumber);
+//console.log(addingNumber(3,2));
