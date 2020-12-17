3f8f8ef81aa91f20e8ca9b4a30c4a69d3b27ad9c
slowy07 <slowy.arfy@gmail.com>
2020-12-18 06:56:36 +0700

    bogo sorting

---
+++ algorithm/sorting/bogoSort.js
@@ -0,0 +1,43 @@
+function Bogosort(arr){
+    var isSorted = function(arr){
+        for(var i = 1; i < arr.length; i++){
+            if (arr[i-1] > arr[i]) {
+                return false;
+            }
+        }
+        return true;
+    };
+
+    function shuffle(arr){
+        var count = arr.length, temp, index;
+
+        while(count > 0){
+            index = Math.floor(Math.random() * count);
+            count--;
+
+            temp = arr[count];
+            arr[count] = arr[index];
+            arr[index] = temp;
+        }
+
+        return arr;
+    }
+
+   function sort(arr){
+        var sorted = false;
+        while(!sorted){
+            arr = shuffle(arr);
+            sorted = isSorted(arr);
+        }
+        return arr;
+    }
+
+    return sort(arr);
+}
+
+
+var array = [2, 7, 8, 5, -1, 4, 1]; 
+console.log("Original Array Elements"); 
+console.log(array); 
+console.log("Sorted Array Elements"); 
+console.log(Bogosort(array));
