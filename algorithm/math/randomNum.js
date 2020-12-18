28d1fe47a37fd26c90061ab5f012c998bd3f23fd
slowy07 <slowy.arfy@gmail.com>
2020-12-18 19:57:05 +0700

    randomNum

---
+++ algorithm/math/randomNum.js
@@ -0,0 +1,9 @@
+const randomNumber = ({ min, max, integer }) => {
+
+    const number = Math.random() * (max - min) + min;
+
+    if (integer) {
+        return Math.round(number);
+    }
+    return number;
+}
