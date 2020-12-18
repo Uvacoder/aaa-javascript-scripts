100e572484cb9d7e968f26cdf3ae1520052362ef
slowy07 <slowy.arfy@gmail.com>
2020-12-18 19:44:03 +0700

    fibonacci recursive

---
+++ algorithm/math/fibonacciRecursive.js
@@ -0,0 +1,14 @@
+function fibonacci(n){
+  if(n<=1)
+    return n;
+  else
+    return fibonacci(n-1) + fibonacci (n-2);
+}
+
+// Test
+
+console.log("The first item of the sequence is:")
+console.log(fibonacci(1))
+
+console.log("The tweleth item of the sequence is:")
+console.log(fibonacci(12))
