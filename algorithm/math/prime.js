1c1d7c93098907aeb8b322e82e6644cba3fa726e
slowy07 <slowy.arfy@gmail.com>
2020-12-18 19:56:50 +0700

    prime

---
+++ algorithm/math/prime.js
@@ -0,0 +1,21 @@
+function isPrime(n){
+  var divisor = 2;
+
+  while (n > divisor){
+    if(n % divisor == 0){
+     return false;
+    }else {
+      divisor++;
+    }
+  }
+  return true;
+}
+
+// Test
+
+console.log("The number 137 is prime:"
+console.log(isPrime(137))
+
+
+console.log("The number 16 is prime:"
+console.log(isPrime(16))
